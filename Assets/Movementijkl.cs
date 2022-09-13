067b20795cb4c9678d2f47ee1e03a1792245b7d6
Brita Munsinger <bmunsing@trinity.edu>
2022-09-12 19:23:32 -0500

    initial commit

---
+++ Assets/Movementijkl.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movementijkl : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        //Get the rigidbody2d of the gameObject this script is assigned to.
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        //Determine the direction of the movement based on user input.
+        float moveDir = Input.GetAxis("Horizontal");
+
+        //Calculate the velocity of the gameObject.
+        //rb.velocity = new Vector2(moveDir * speed, rb.velocity.y);
+
+        // Your jump code:
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            pos.y += jumpHeight;
+        }
+
+
+
+        if (Input.GetKey("i"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("k"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("l"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("j"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+       /* if (Input.GetMouseButton(1))
+        {
+            pos.x = Random.Range(-4.0F, 4.0F);
+            pos.y = Random.Range(-2.0F, 2.0F);
+
+        } */
+
+        transform.position = pos;
+    }
+
+}
+
+
---
+++ Assets/Resetbutton1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resetbutton1 : MonoBehaviour
+{
+    public GameObject redsquare;
+    public GameObject greensquare;
+
+    public void resetsquares()
+    {
+        redsquare.transform.position = new Vector2(-1, 0);
+        greensquare.transform.position = new Vector2(1, 0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
